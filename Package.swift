ffb1318ed0713b3914be7c522790edec613ab301
Vladyslav Kudelia <macv8@Vladyslavs-MacBook-Pro.local>
2026-03-23 15:55:30 -0700

    Update binary url/checksum

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AstraNavKit",
-            url: "https://astra-navigation.repo.sonatype.app/repository/astranav-raw/AstraNavKit/1.0.5/AstraNavKit.xcframework.zip",
-            checksum: "3d66dfc64fdd954dba5cf873ba9b42ed3417baa6042ceab8060070f6a72dfeb1"
+            url: "https://astra-navigation.repo.sonatype.app/repository/astranav-raw/AstraNavKit/test/0.0.1/AstraNavKit.xcframework.zip",
+            checksum: "97684cabae13348c119fc9644812edc1ca9e9d1df07075709803c99e8506ae1d"
         )
     ]
 )
